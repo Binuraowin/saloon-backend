98060c584b570358fc0028c6a4f00037a3b277bf
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-02-16 23:37:36 +0530

    Update employee.js

--- api/models/employee.js
+++ api/models/employee.js
@@ -4,6 +4,7 @@ const employeeSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name:{type:String, required: true},
     service:{type:String, required: true},
+    jobRole:{type:String, required: true},
     workingHours:{type:Number, required: true},
     workedHours:{type:Number, required: true},
     sallery: { type: Number, required: true }
